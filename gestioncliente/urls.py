53b236c7a2ddbd2cd574129ce0ba88741c4fff4d
iRaiiDeer <128615433+iRaiiDeer@users.noreply.github.com>
2023-06-09 17:06:06 -0400

    Add files via upload

--- gestioncliente/urls.py
+++ gestioncliente/urls.py
@@ -3,6 +3,7 @@ from gestioncliente import views #importará los métodos que generemos en nuest
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
+from gestioncliente import views
 gestioncliente_urlpatterns = [
 
     path('clientes_main/',views.clientes_main,name="clientes_main"),
@@ -16,6 +17,7 @@ gestioncliente_urlpatterns = [
     path('carga_masiva_cliente/',views.carga_masiva_cliente,name="carga_masiva_cliente"),
     path('cliente_carga_masiva_save/',views.cliente_carga_masiva_save,name="cliente_carga_masiva_save"),
     path('import_file_cliente/',views.import_file_cliente,name="import_file_cliente"),
-
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('generar_reporte_cliente/', views.generar_reporte_cliente, name='generar_reporte_cliente'),
     
     ]
--- gestioncliente/views.py
+++ gestioncliente/views.py
@@ -28,10 +28,132 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.platypus import Table, TableStyle, Paragraph
 from reportlab.platypus.flowables import Spacer
+import plotly.graph_objects as go
 
 
+from reportlab.lib import colors
+from reportlab.pdfgen import canvas
+from django.template.loader import get_template
+from django.template import Context
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle
+from datetime import date
+from django.urls import reverse
+
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
+@login_required
+def listar_cliente(request):
+    busqueda = request.GET.get("buscar")
+    clientes = Cliente.objects.all()
+
+    if busqueda:
+        clientes = clientes.filter(nombre1__icontains=busqueda)
+
+    if 'generar_reporte_cliente' in request.GET:
+        return redirect(reverse('generar_reporte_cliente') + '?buscar=' + busqueda)
+    
+    if not busqueda:
+        clientes = Cliente.objects.all()
+
+    return render(request, 'gestioncliente/listar_cliente.html', {'clientes': clientes, 'buscar': busqueda})
+
+
+
+
+@login_required
+
+def generar_reporte_cliente(request):
+    busqueda = request.GET.get("buscar")
+    clientes = Cliente.objects.all()
+    if busqueda:
+        clientes = clientes.filter(nombre1__icontains=busqueda)
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="reporte_clientes.pdf"'
+    doc = SimpleDocTemplate(response, pagesize=letter)
+    data = []
+    data.append(['Nombre', 'Segundo nombre', 'Apellido', 'Segundo apellido', 'Correo','Celular','Edad', 'Codigo postal'])
+    for cliente in clientes:
+        data.append([cliente.nombre1, cliente.nombre2, cliente.apellido1, cliente.apellido2, cliente.correo_electronico, cliente.celular, cliente.edad])
+    # Crear los estilos de tabla y párrafo
+    styles = getSampleStyleSheet()
+    style_table = TableStyle([
+    ('BACKGROUND', (0, 0), (-1, 0), '#CCCCCC'),
+    ('TEXTCOLOR', (0, 0), (-1, 0), '#000000'),
+    ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+    ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+    ('FONTSIZE', (0, 0), (-1, 0), 12),
+    ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+    ('BACKGROUND', (0, 1), (-1, -1), '#EEEEEE'),
+    ('COLWIDTHS', (0, 0), (-1, -1), 60),  # Ajusta el ancho de todas las columnas
+    ])
+    style_paragraph = ParagraphStyle(
+        name='BodyText',
+        parent=styles['Normal'],
+        spaceBefore=6,
+        spaceAfter=6,
+    )
+
+    # Crear la tabla con los datos y aplicar estilos
+    table = Table(data)
+    table.setStyle(style_table)
+
+    # Agregar la tabla al documento
+    elements = [Paragraph('Informe de Clientes', styles['Heading1'])]
+    elements.append(Paragraph('Fecha: ' + date.today().strftime('%d/%m/%Y'), style_paragraph))
+    elements.append(Paragraph('Listado de Clientes:', styles['Heading3']))
+    elements.append(table)
+
+    doc.build(elements)
+
+    return response
 
 #CLIENTE
+def dashboard(request):
+    # Obtener todos los clientes
+    clientes = Cliente.objects.all()
+
+    # Grafico de pastel: Distribución de edades de los clientes
+    edades = [cliente.edad for cliente in clientes if cliente.edad is not None]
+    labels = ['<18', '18-25', '26-35', '36-50', '>50']
+    values = [
+        len([edad for edad in edades if edad < 18]),
+        len([edad for edad in edades if 18 <= edad <= 25]),
+        len([edad for edad in edades if 26 <= edad <= 35]),
+        len([edad for edad in edades if 36 <= edad <= 50]),
+        len([edad for edad in edades if edad > 50])
+    ]
+
+    fig1 = go.Figure(data=[go.Pie(labels=labels, values=values)])
+    fig1.update_layout(title='Distribución de edades de los clientes')
+
+    # Grafico de barras: Cantidad de clientes por mes de creación
+    meses_creacion = [cliente.created.month for cliente in clientes if cliente.created is not None]
+    labels2 = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    values2 = [meses_creacion.count(i) for i in range(1, 13)]
+
+    fig2 = go.Figure(data=go.Bar(x=labels2, y=values2))
+    fig2.update_layout(title='Cantidad de clientes por mes de creación')
+
+    # Datos útiles
+    cantidad_clientes = len(clientes)
+    cliente_mas_reciente = max(clientes, key=lambda cliente: cliente.created)
+    cliente_mas_antiguo = min(clientes, key=lambda cliente: cliente.created)
+
+    context = {
+        'plot_div1': fig1.to_html(full_html=False),
+        'plot_div2': fig2.to_html(full_html=False),
+        'cantidad_clientes': cantidad_clientes,
+        'cliente_mas_reciente': cliente_mas_reciente,
+        'cliente_mas_antiguo': cliente_mas_antiguo,
+    }
+
+    return render(request, 'gestioncliente/dashboard_3.html', context)
+
 @login_required
 def clientes_main(request):
     profile = Profile.objects.get(user_id=request.user.id)
@@ -51,15 +173,24 @@ def agregar_cliente(request):
         if formulario.is_valid():
             formulario.save()
             messages.add_message(request, messages.INFO, 'cliente creado!')
+            return redirect('listar_cliente')
     return render(request, 'gestioncliente/agregar_cliente.html',data)
 
-@login_required
-def listar_cliente(request):
-     clientes = Cliente.objects.all()
-     data={
-         'clientes': clientes
-     }
-     return render(request, 'gestioncliente/listar_cliente.html',data)
+
+def buscar_cliente(request):
+    if request.method == 'GET':
+        nombre = request.GET.get('nombre')
+
+        # Realizar la búsqueda de clientes por nombre
+        clientes = Cliente.objects.filter(nombre__icontains=nombre)
+
+        data = {
+            'clientes': clientes,
+            'nombre_busqueda': nombre
+        }
+        return render(request, 'gestioncliente/buscar_cliente.html', data)
+
+    return redirect('listar_cliente')
 @login_required
 def actualizar_cliente(request, id):
     cliente = get_object_or_404(Cliente, id=id)
