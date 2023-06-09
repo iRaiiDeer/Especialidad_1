ca5300020a93233f6a3fe397d218e7b5d6e5e6ca
iRaiiDeer <128615433+iRaiiDeer@users.noreply.github.com>
2023-06-09 17:01:23 -0400

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import inlineformset_factory
-from .models import Categoria
-
-
-class CategoriaForm(forms.ModelForm):
-    class Meta: 
-        model = Categoria
-        fields = '__all__'
